3575296822e43409573c4987cce74d1b82488564
Pavlo Liulia <pavloblack@hotmail.com>
2016-11-07 22:25:01 +0100

    init

---
+++ src/lib/users.js
@@ -0,0 +1,41 @@
+import User from '../models/User';
+
+class Users {
+  getList() {
+    return User.find({}).exec();
+  }
+
+  signup(username, password) {
+    console.log(username);
+    console.log(password);
+    var user = new User({
+      username: username,
+      password: password
+    });
+
+    return user.save();
+  }
+
+  login(username, password, done) {
+    var findUser = User.findOne({ username: username }).exec();
+    var user = {};
+    findUser.then((data) => {
+      user = data;
+      return user.validPassword(password);
+    }).then(()=> {
+      return done(null, user);
+    }, (err)=> {
+      return done(null, false, { message: 'Username and password do not match.' });
+    });
+  }
+
+  updateMail(id, mail) {
+    const getUser = User.findOne({_id: id}).exec();
+    return getUser.then((user) => {
+      user.mail = mail;
+      return user.save();
+    });
+  }
+}
+
+export default Users;
